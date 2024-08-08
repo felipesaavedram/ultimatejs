33cd83bd44fc141c83034fc9eac0fed205116bb9
felipesaavedram <saavedra.mfelipe@gmail.com>
2024-08-08 16:09:53 -0400

    Se agrega el archivo de js 08-funciones y se completa la definicion de arrays

---
+++ 01-tipos/07-arrays.js
@@ -0,0 +1,10 @@
+let animales  = ['chanchito','caballo','wonejo'];
+
+animales[3] = 'Dragón'; 
+animales[10] = 'Pudú' 
+
+console.log (animales);
+
+console.log(typeof animales);
+
+console.log(animales.length);

5dd714f717136ad2ded51ab808373d8e60bb8c71
felipesaavedram <saavedra.mfelipe@gmail.com>
2024-09-14 11:59:05 -0400

    Se agregan los ultimos cambios antes de continuar

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,13 @@
+let a = 10;
+
+//Operadores Relacionales
+console.log(a > 5); // a mayor que 5
+console.log(a >= 5);// a mayor o igual que 5
+console.log(a < 5);// a menor que 5
+console.log(a <= 5); // a menor o igual que 5
+
+//Operadores de Igualdad
+console.log(a == 10); // a es igual a 10  (Evitar usar estos, fuente de errores)
+console.log(a != 10);// a es distinto a 10  (Evitar usar estos, fuente de errores)
+console.log(a === '10'); // a es igual que 10 + a tiene el mismo tipo de dato que 10
+console.log(a !== '10'); // a es igual que 10 + a tiene distinto tipo de dato que 10
---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,18 @@
+// AND, OR, NOT
+
+// AND &&
+
+let mayor = false;
+let suscrito = true;
+
+// console.log(true && true);
+// console.log(false && true);
+
+console.log('Operador AND',mayor && suscrito)
+
+
+// AND ||
+console.log('Operador OR',mayor || suscrito)
+
+// NOT !
+console.log('Operador NOT',!mayor);

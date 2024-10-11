84f2b25abd5d64f05da0448a1cf973fb16834370
felipesaavedram <saavedra.mfelipe@gmail.com>
2024-10-11 12:44:34 -0400

    Se suben los cambios actualizados

---
+++ 03-control-flujo/07-for-of.js
@@ -0,0 +1,12 @@
+// while
+// Do while
+// for
+
+let animales = ['Chanchito feliz', 'Dragón', 'Perrito'];
+
+for (let animal of animales) {
+    console.log(animal);
+}
+
+let i = 0;
+while (i < animales.length) {   }
